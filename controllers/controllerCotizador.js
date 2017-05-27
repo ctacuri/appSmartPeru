64f9efbe165089ceca9428d2d56f211c5e297be8
ctacuri <cesar_hbk@hotmail.com>
2017-05-26 23:05:32 -0500

    Add Controllers/Models

---
+++ controllers/controllerCotizador.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Cesar on 26/05/17.
+ */
+var model = require("../models/modelCotizador");
+var path = require("path");
+//var sleep = require('sleep');
+
+var controller = function(){}
+
+
+
+module.exports = controller;
---
+++ models/modelCobranza.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Cesar on 26/05/17.
+ */
+var conn = require("../connection/connMYSQL");
+
+var model = function(){};
+
+
+
+module.exports = model;
